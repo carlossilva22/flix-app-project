65e31e41d250518560bde41cb7aadac919f105e3
Carlos Silva <55637586+carlossilva22@users.noreply.github.com>
2024-05-26 21:15:45 +0100

    change 6

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const global = {
     type: '',
     page: 1,
     totalPages: 1,
+    totalResults: 0,
   },
   api: {
     apiKey: '3fd2be6f0c70a2a598f084ddfb75487c',
@@ -242,8 +243,13 @@ async function search() {
 
   if (global.search.term !== '' && global.search.term !== null) {
     //@todo - make request and display results
-    const { results, total_pages, page } = await searchAPIDate();
+    const { results, total_pages, page, total_results } = await searchAPIDate();
     searchAPIDate();
+
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
+
     if (results.length === 0) {
       showAlert('No results found.');
       return;
@@ -257,6 +263,10 @@ async function search() {
 }
 
 function displaySearchResults(results) {
+  //Clear search results
+  document.querySelector('#search-results').innerHTML = '';
+  document.querySelector('#search-results-heading').innerHTML = '';
+  document.querySelector('#pagination').innerHTML = '';
   results.forEach((result) => {
     const div = document.createElement('div');
     div.classList.add('card');
@@ -290,8 +300,14 @@ function displaySearchResults(results) {
     </p>
   </div>
 `;
+
+    document.querySelector('#search-results-heading').innerHTML = `
+<h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>
+`;
     document.querySelector('#search-results').appendChild(div);
   });
+
+  displayPagination();
 }
 
 //Display Slider movies
@@ -312,6 +328,42 @@ async function displaySlider() {
     document.querySelector('.swiper-wrapper').appendChild(div);
 
     initSwiper();
+  });
+}
+
+//Create & display pagination for search
+function displayPagination() {
+  const div = document.createElement('div');
+  div.classList.add('pagination');
+  div.innerHTML = `  
+  <button class="btn btn-primary" id="prev">Prev</button>
+  <button class="btn btn-primary" id="next">Next</button>
+  <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>`;
+
+  document.querySelector('#pagination').appendChild(div);
+
+  //Disable pre button if on first page
+  if (global.search.page === 1) {
+    document.querySelector('#prev').disabled = true;
+  }
+
+  //Disable next button if on last page
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector('#next').disabled = true;
+  }
+
+  //Next Page
+  document.querySelector('#next').addEventListener('click', async () => {
+    global.search.page++;
+    const { results, total_pages } = await searchAPIDate();
+    displaySearchResults(results);
+  });
+
+  //prev Page
+  document.querySelector('#prev').addEventListener('click', async () => {
+    global.search.page--;
+    const { results, total_pages } = await searchAPIDate();
+    displaySearchResults(results);
   });
 }
 
@@ -365,7 +417,7 @@ async function searchAPIDate() {
   showSpinner();
 
   const response = await fetch(
-    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
   );
 
   const data = await response.json();
